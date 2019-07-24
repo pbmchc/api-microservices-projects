f51c6d1a7e177193c119c1539a974b915b782bb9
PBM <pbmalecki@wp.pl>
2019-07-24 15:56:07 +0200

    Added initial version of exercise tracker project

---
+++ exercise-tracker/server.js
@@ -0,0 +1,46 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+
+const cors = require('cors');
+
+const mongoose = require('mongoose');
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+app.get('/', (_, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.use((req, res, next) => {
+  return next({ status: 404, message: 'not found' });
+});
+
+app.use((err, req, res, next) => {
+  let errCode, errMessage;
+
+  if (err.errors) {
+    errCode = 400;
+    const keys = Object.keys(err.errors);
+
+    errMessage = err.errors[keys[0]].message;
+  } else {
+    errCode = err.status || 500;
+    errMessage = err.message || 'Internal Server Error';
+  }
+
+  res.status(errCode).type('txt')
+    .send(errMessage);
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+});
