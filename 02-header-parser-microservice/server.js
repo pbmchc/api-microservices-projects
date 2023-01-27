47ec0d7ac8c070f73e56401bd922fc01194c0ed0
pbmchc <pbmalecki@wp.pl>
2023-01-27 20:05:21 +0100

    chore: use latest stable express version in header parser microservice

--- 02-header-parser-microservice/server.js
+++ 02-header-parser-microservice/server.js
@@ -1,20 +1,22 @@
-const PORT = process.env.PORT || 3000;
 const cors = require('cors');
 const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(cors({ optionSuccessStatus: 200 }));
 app.use(express.static('public'));
 app.enable('trust proxy');
 
-app.get("/", function (_, res) {
+app.get('/', (_, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get(
   '/api/whoami',
   infoExtractor,
-  ({ userInfo }, res) => res.json(userInfo)
+  ({ userInfo }, res) => { res.json(userInfo) }
 );
 
 function infoExtractor(req, _, next) {
@@ -25,9 +27,10 @@ function infoExtractor(req, _, next) {
     language: req.header('Accept-Language'),
     software: req.header('User-Agent')
   };
+
   next();
 }
 
-const listener = app.listen(PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(PORT, () => {
+  console.log(`Your app is listening on port ${PORT}`);
 });
