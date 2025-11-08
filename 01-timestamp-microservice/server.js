e9433339a02f47d199b0283ac0f84a1c0b2a374f
pbmchc <pbmalecki@wp.pl>
2025-11-08 14:08:55 +0100

    chore: bump node in timestamp microservice

--- 01-timestamp-microservice/server.js
+++ 01-timestamp-microservice/server.js
@@ -1,37 +1,32 @@
+import cors from 'cors';
+import express from 'express';
+
 const PORT = process.env.PORT || 3000;
-const cors = require('cors');
-const express = require('express');
+
 const app = express();
 
-app.use(cors({ optionSuccessStatus: 200 }));
+app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static('public'));
 
-app.get("/", function (_, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get('/', function (_, res) {
+  res.sendFile('views/index.html', { root: import.meta.dirname });
 });
 
-app.get(
-  '/api/timestamp/:date_string?',
-  dateValidator,
-  ({ unix, utc }, res) => {
-    res.json({ unix, utc });
-  }
-);
+app.get('/api/timestamp/{:date_string}', dateValidator, ({ unix, utc }, res) => {
+  res.json({ unix, utc });
+});
 
 function dateValidator(req, res, next) {
-  let date;
   const { date_string } = req.params;
 
+  let date;
   if (date_string) {
-    date = !isNaN(date_string)
-      ? new Date(parseInt(date_string * 1000, 10))
-      : new Date(date_string);
+    date = !isNaN(date_string) ? new Date(parseInt(date_string * 1000, 10)) : new Date(date_string);
   } else {
     date = new Date();
   }
 
   const unix = date.getTime();
-
   if (!unix) {
     res.send({ error: 'Invalid Date' });
   }
@@ -41,6 +36,6 @@ function dateValidator(req, res, next) {
   next();
 }
 
-const listener = app.listen(PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(PORT, function () {
+  console.log(`Your app is listening on port ${PORT}`);
 });
